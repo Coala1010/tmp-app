b4eca8e5cc02b00ac6eed75bd9c7e4df7a7a6cb5
Andriy Gavriluk <andriy.gavriluk@altexsoft.com>
2020-04-08 21:53:52 +0300

    Cleanup

--- providers/UserLessonsProvider.tsx
+++ providers/UserLessonsProvider.tsx
@@ -7,7 +7,6 @@ export default function UserLessonsProvider(userToken: string, unitId:number, re
     })
     .then((response) => {
         response.json().then(json => {
-            console.log(json);
             resultCallback(json);
         });
     });
