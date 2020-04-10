e1240e6148fa1f31e56450814846e14369159c73
Andriy Gavriluk <andriy.gavriluk@altexsoft.com>
2020-04-10 12:46:11 +0300

    Remove test data

--- screens/activities/MultichoiceActivity.tsx
+++ screens/activities/MultichoiceActivity.tsx
@@ -15,28 +15,6 @@ export default function MultichoiceActivityScreen({ navigation, route }) {
     React.useEffect(() => {
         try {
             getMultichoice(1).then((res) => {
-                res[1] = {
-                    hint: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porttitor elit quis sem ultricies, vel ultrices lectus viverra. Sed vel metus porttitor, bibendum erat in, imperdiet erat. Sed a sapien nulla. Curabitur scelerisque urna sit amet felis pellentesque, vitae ornare est porta. Nulla",
-                    id: 4,
-                    question: "ما هي عاصمة السعودية ؟",
-                    title: "الدرس الخامس",
-                    userAnswerId: 1,
-                    activityStatus: true,
-                    answerId: 2,
-                    userMultichoiceActivityGroupId: 1,
-                    answers: [
-                        { id: 1, answer: 'الرياض', isCorrect: true, },
-                        { id: 2, answer: 'المدينة' },
-                        { id: 3, answer: 'مكة' },
-                        { id: 4, answer: 'الرياض' },
-                        { id: 5, answer: 'الرياض' },
-                        { id: 6, answer: 'الرياض' },
-                        { id: 7, answer: 'الرياض' },
-                        { id: 8, answer: 'الرياض' },
-                        { id: 9, answer: 'الرياض' },
-                    ],
-                };
-
                 setActivityData(res);
             });
         } catch (err) {
